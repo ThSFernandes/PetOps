6783db2904257eec97e00e42cc48776a37b22398
ThSFernandes <Thsalles99@gmail.com>
2025-07-08 10:56:37 -0300

    feat : iniciando estrutura do projeto

---
+++ src/main/java/petOps/com/petshop/PetOpsApplication.java
@@ -0,0 +1,13 @@
+package petOps.com.petshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetOpsApplication.class, args);
+	}
+
+}

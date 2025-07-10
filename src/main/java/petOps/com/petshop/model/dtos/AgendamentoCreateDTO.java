3f107f21ccad9566d4125b6452d9b39f799d4742
JoaoVSMonteiro <joao_dj58@hotmail.com>
2025-07-10 14:25:02 -0300

    feat: Adiciona Dtos e mapper agendamento

---
+++ src/main/java/petOps/com/petshop/model/dtos/AgendamentoCreateDTO.java
@@ -0,0 +1,27 @@
+package petOps.com.petshop.model.dtos;
+
+import petOps.com.petshop.model.entity.Funcionario;
+import petOps.com.petshop.model.entity.Pet;
+import petOps.com.petshop.model.entity.Servico;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+public class AgendamentoCreateDTO {
+
+    private Long id_agendamento;
+
+    private LocalDate data_marcada;
+
+    private String status;
+
+    private String observacoes;
+
+    private Pet pet;
+
+    private Funcionario funcionario_veterinario;
+
+    private Funcionario funcionario_groomer;
+
+    private Set<Servico> servicos_adicionais;
+}

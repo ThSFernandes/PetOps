846eb4f97f1ba9c8aec4e9c16413a3e51a91a83f
JoaoVSMonteiro <joao_dj58@hotmail.com>
2025-07-11 10:22:26 -0300

    feat: Adiciona Dtos e mapper tutor

---
+++ src/main/java/petOps/com/petshop/model/mapper/TutorMapper.java
@@ -0,0 +1,17 @@
+package petOps.com.petshop.model.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import petOps.com.petshop.model.dtos.TutorCreateDTO;
+import petOps.com.petshop.model.dtos.TutorDTO;
+import petOps.com.petshop.model.entity.Tutor;
+
+@Mapper(componentModel = "spring")
+public interface TutorMapper {
+
+    TutorDTO toDto(Tutor tutor);
+
+    @Mapping(target = "id_tutor", ignore = true)
+    Tutor toEntity(TutorCreateDTO TutorCreateDTO);
+
+}
